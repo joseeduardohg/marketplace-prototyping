63627de3f8b7794b98848fcaf945c5a225c9ad10
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-04-11 16:56:57 -0500

    Add multiple product cards with TypeScript

---
+++ src/tienda/tienda.ts
@@ -0,0 +1,54 @@
+const productContainer = document.querySelector(
+  '.container__products',
+) as HTMLDivElement;
+
+const productCard = document.createElement('template');
+
+const productCardTemplate = `
+          <div class="card">
+            <div class="card__header">
+              <img
+                class="card__image"
+                src="/src/assets/images/placeholder-image.webp"
+                alt="Producto 1"
+              />
+            </div>
+
+            <div class="card__content">
+              <h2 class="card__heading">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Accusantium consequuntur hic magni necessitatibus aut quisquam
+                odit ducimus consectetur perspiciatis iure dolores aliquam id
+                fuga vel earum, corrupti accusamus? Sed, aliquid!
+              </h2>
+
+              <div>
+                <i>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="card__icon lucide lucide-heart-icon lucide-heart"
+                  >
+                    <path
+                      d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
+                    />
+                  </svg>
+                </i>
+                <span class="card__price">$10.00</span>
+              </div>
+            </div>
+          </div>
+`;
+
+productCard.innerHTML = productCardTemplate;
+for (let i = 0; i < 14; i++) {
+  const clone = productCard.content.cloneNode(true) as DocumentFragment;
+  productContainer.appendChild(clone);
+}
